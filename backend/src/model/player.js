5145630cf9eb0ccaf7e237f4343f4991b1c46a5a
Shinatip Pattanapitoon <warewolfth@gmail.com>
2024-04-14 00:05:35 +0700

    Implement game's lobby routes

--- backend/src/model/player.js
+++ backend/src/model/player.js
@@ -4,18 +4,17 @@ const Ship = require('./ship')
 const playerSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
     },
     status: {
         type: String,
         immutable: true,
         default: "alive"
     },
-    board: { 
-        type: Array,
-        required: true
-    },
-    ship: [Ship]
+    // board: { 
+    //     type: Array,
+    //     required: true
+    // },
+    // ship: [Ship]
 })
 
 module.exports = playerSchema
--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const gameRouter = require('./routes/game')
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
